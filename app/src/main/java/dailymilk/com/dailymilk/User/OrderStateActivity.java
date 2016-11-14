b79b8a14e698b239074a947f02c7c1c5dae97c66
Lukas Gabsi <lukas.gabsi@tele2.at>
2016-11-14 13:39:34 +0100

    Initial Commit
    try #02

---
+++ app/src/main/java/dailymilk/com/dailymilk/User/OrderStateActivity.java
@@ -0,0 +1,13 @@
+package dailymilk.com.dailymilk.User;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderStateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_state);
+    }
+}
