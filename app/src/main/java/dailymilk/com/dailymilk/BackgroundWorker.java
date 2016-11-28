bdea6ce358c4b91e01cdd79a311647c7ab21492c
Lukas Gabsi <lukas.gabsi@tele2.at>
2016-11-28 13:41:50 +0100

    Login
    added LoginActivity
    tested and works

---
+++ app/src/main/java/dailymilk/com/dailymilk/BackgroundWorker.java
@@ -0,0 +1,93 @@
+package dailymilk.com.dailymilk;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.renderscript.ScriptGroup;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+/**
+ * Created by Admin on 28.11.2016.
+ */
+
+public class BackgroundWorker extends AsyncTask<String, Void, String> {
+    Context context;
+    AlertDialog alertDialog;
+    BackgroundWorker (Context ctx){
+        context = ctx;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String login_url = "http://dailymilk.tk/login.php";
+        if(type.equals("login")){
+            try {
+                String user_name = params[1];
+                String password = params[2];
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream oS = httpURLConnection.getOutputStream();
+                BufferedWriter bW = new BufferedWriter(new OutputStreamWriter(oS, "UTF-8"));
+                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
+                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bW.write(post_data);
+                bW.flush();
+                bW.close();
+                oS.close();
+                InputStream iS = httpURLConnection.getInputStream();
+                BufferedReader bR = new BufferedReader(new InputStreamReader(iS, "iso-8859-1"));
+                String result="";
+                String line="";
+                while((line = bR.readLine())!= null){
+                    result += line;
+                }
+                bR.close();
+                iS.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+}
+
+
+
