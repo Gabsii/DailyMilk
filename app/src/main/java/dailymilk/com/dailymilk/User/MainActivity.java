c595dff7ecfc3fe7bb2cc6babbad3c7e310d550a
Lukas Gabsi <lukas.gabsi@tele2.at>
2016-12-05 14:23:09 +0100

    set Username

--- app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
+++ app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import dailymilk.com.dailymilk.LoginActivity;
 import dailymilk.com.dailymilk.R;
@@ -17,7 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView DisplayUser = (TextView) findViewById(R.id.Username);
         String username = getIntent().getStringExtra(EXTRA_MESSAGE);
+
+        DisplayUser.setText(username);
     }
 
     public void onOrder(View view) {
