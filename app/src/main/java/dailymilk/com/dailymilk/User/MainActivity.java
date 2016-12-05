016a45b03a06171c6486243836ee32f31258183b
StinkyBen <benedikt.fink@student.htldornbirn.at>
2016-12-05 13:56:19 +0100

    MainActivity / OrderStateActivity
    MainActivity Layout and intent onButtonClick --> OrderStateAcivity

--- app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
+++ app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
@@ -1,7 +1,9 @@
 package dailymilk.com.dailymilk.User;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import dailymilk.com.dailymilk.R;
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onOrder(View view) {
+        Intent intent = new Intent(this, OrderStateActivity.class);
+        startActivity(intent);
     }
 }
