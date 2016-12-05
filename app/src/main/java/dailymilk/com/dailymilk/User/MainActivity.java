ca153a5916b195ca9d2d5e9c01026bf756d2e5d1
Lukas Gabsi <lukas.gabsi@tele2.at>
2016-12-05 13:50:26 +0100

    switch to MainActivity
    After logging in, you switch to the MainActivity to take the order

--- app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
+++ app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
@@ -1,15 +1,21 @@
 package dailymilk.com.dailymilk.User;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dailymilk.com.dailymilk.LoginActivity;
 import dailymilk.com.dailymilk.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "Username";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String username = getIntent().getStringExtra(EXTRA_MESSAGE);
     }
 }
