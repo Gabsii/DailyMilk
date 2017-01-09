fc24518ec90b6d973b9ff7f5328cf90fca7c998c
StinkyBen <benedikt.fink@student.htldornbirn.at>
2017-01-09 13:20:51 +0100

    changed orders icon / removed FUN

--- app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
+++ app/src/main/java/dailymilk/com/dailymilk/User/MainActivity.java
@@ -2,7 +2,6 @@ package dailymilk.com.dailymilk.User;
 
 import android.app.ActionBar;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -81,18 +80,12 @@ public class MainActivity extends AppCompatActivity {
                 BackgroundWorker backgroundWorker = new BackgroundWorker(this);
                 backgroundWorker.execute("view",username);
                 break;
-            case R.id.song:
-                final MediaPlayer mp = MediaPlayer.create(this, R.raw.song);
-                mp.start();
         }
         return true;
     }
 
     public void onOrder(View view) {
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
         BackgroundWorker backgroundWorker = new BackgroundWorker(this);
         backgroundWorker.execute("order",username, orderedItem);
     }
