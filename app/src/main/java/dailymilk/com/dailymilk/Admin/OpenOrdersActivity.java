2746c1e773d03690f63d0845507b7c6fd329ad73
Lukas Gabsi <lukas.gabsi@tele2.at>
2017-02-18 01:43:43 +0100

    Able to remove Orders
    Finally added a way to update the OrderState of delivered Orders. Also
    added some lines to make the gradle build faster and added some
    libraries for eventual http configuration.

--- app/src/main/java/dailymilk/com/dailymilk/Admin/OpenOrdersActivity.java
+++ app/src/main/java/dailymilk/com/dailymilk/Admin/OpenOrdersActivity.java
@@ -1,5 +1,6 @@
 package dailymilk.com.dailymilk.Admin;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,12 +8,17 @@ import android.util.SparseBooleanArray;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 
+import dailymilk.com.dailymilk.BackgroundRemover;
 import dailymilk.com.dailymilk.R;
 
 public class OpenOrdersActivity extends AppCompatActivity {
@@ -23,6 +29,8 @@ public class OpenOrdersActivity extends AppCompatActivity {
     ArrayAdapter<String> adapter;
     ListView chl;
     private ArrayList<String> orderArray;
+    private ArrayList<String> idArray;
+    private ArrayList<String> remidArray = new ArrayList<>();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,25 +47,56 @@ public class OpenOrdersActivity extends AppCompatActivity {
     public void populate(){
         String [] result = getIntent().getStringExtra(EXTRA_RESULT).split(";");
         orderArray = new ArrayList<>(Arrays.asList(result));
+        idArray = new ArrayList<>();
+        for(int i = 0; orderArray.size() > i ; i++) {
+            String str = orderArray.get(i).toString();
+            String numberonly = str.replaceAll("[^0-9]+", "").trim();
+            idArray.add(i, numberonly);
+            String item = str.replaceAll("[^A-Za-z\\s]","");
+            orderArray.set(i, item);
+            //Toast.makeText(getApplicationContext(), item, Toast.LENGTH_SHORT).show();
+        }
+
         chl = (ListView) findViewById(R.id.checkableList);
 
         chl.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         adapter = new ArrayAdapter<>(this, R.layout.rowlayout, R.id.txt_lan, orderArray);
         chl.setAdapter(adapter);
+
+        //Toast.makeText(getApplicationContext(), idArray.get(109).toString(), Toast.LENGTH_LONG).show();
     }
 
     public void removeSelectedItems(MenuItem menuItem){
+
         SparseBooleanArray checkedItems = chl.getCheckedItemPositions();
         int itemCount = chl.getCount();
 
+
+        int j = 0;
         for(int i = itemCount-1; i>=0;i--){
             if(checkedItems.get(i)){
+                //remidArray.add(j, idArray.get(i).toString());
+                sendRemovedIds(idArray.get(i).toString());
+                idArray.remove(i);
                 adapter.remove(orderArray.get(i));
+                //j++;
+
+
             }
         }
+
+        //AlertDialog alertDialog;
+        /*alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("removed Orders");
+        alertDialog.setMessage(remidArray.toString());
+        alertDialog.show();*/
+
+        //sendRemovedIds();
         checkedItems.clear();
+        remidArray.clear();
         adapter.notifyDataSetChanged();
     }
+
 
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -78,4 +117,19 @@ public class OpenOrdersActivity extends AppCompatActivity {
         }
         return true;
     }
+
+    public void sendRemovedIds(String id) {
+        BackgroundRemover backgroundRemover = new BackgroundRemover(this);
+        /*AlertDialog alertDialog;
+        alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("removed Orders");
+        alertDialog.setMessage(remidArray.toString());
+        alertDialog.show();
+        ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        for(int i = 0; i>remidArray.size();i++){
+            nameValuePairs.add(new BasicNameValuePair("list[]", remidArray.get(i)));
+        }*/
+        backgroundRemover.execute(id);
+    }
+
 }
---
+++ app/src/main/java/dailymilk/com/dailymilk/BackgroundRemover.java
@@ -0,0 +1,110 @@
+package dailymilk.com.dailymilk;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+
+import dailymilk.com.dailymilk.Admin.OpenOrdersActivity;
+import dailymilk.com.dailymilk.User.MainActivity;
+
+import static dailymilk.com.dailymilk.Admin.OpenOrdersActivity.EXTRA_USERNAME;
+
+
+/**
+ * Created by Gabsii on 28.11.2016.
+ */
+
+public class BackgroundRemover extends AsyncTask<String, Void, String> {
+    Context context;
+    AlertDialog alertDialog;
+    public BackgroundRemover(Context ctx){
+        context = ctx;
+    }
+    String list;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Checking Status");
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        list = params[0];
+        String check_url = "http://dailymilk.tk/check.php";
+        String result;
+            try {
+                URL url = new URL(check_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream oS = httpURLConnection.getOutputStream();
+                BufferedWriter bW = new BufferedWriter(new OutputStreamWriter(oS, "UTF-8"));
+                String post_data = URLEncoder.encode("list", "UTF-8") + "=" + URLEncoder.encode(list.toString(), "UTF-8");
+                bW.write(post_data);
+                bW.flush();
+                bW.close();
+                oS.close();
+                InputStream iS = httpURLConnection.getInputStream();
+                BufferedReader bR = new BufferedReader(new InputStreamReader(iS, "iso-8859-1"));
+                result = "";
+                String line = "";
+                while ((line = bR.readLine()) != null) {
+                    result += line;
+                }
+                bR.close();
+                iS.close();
+                httpURLConnection.disconnect();
+
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        Toast.makeText(context, "sucess", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+
+}
+
+
+
+
